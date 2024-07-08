7a3a2cc8d608b9acda1e769688e24ec3366f5917
dgf0020 <tmxlcl0020@naver.com>
2024-07-08 15:16:33 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 236 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = { 1,1,2,2,2,8 };
+		
+		for (int i = 0; i < arr.length; i++) {
+			int n = sc.nextInt();
+			int ans = arr[i] - n;
+			System.out.print(ans + " ");
+		}
+		
+		sc.close();
+	}
+}
