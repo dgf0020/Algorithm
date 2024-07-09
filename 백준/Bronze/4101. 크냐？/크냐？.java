f072b9aab5bdece5c659e3e8f5783c05bcca4266
dgf0020 <tmxlcl0020@naver.com>
2024-07-09 13:16:33 +0900

    [Bronze V] Title: 크냐?, Time: 196 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if (a == 0 && b == 0) {
+				break;
+			}
+			
+			String ans = "No";
+			
+			if (a > b) {
+				ans = "Yes";
+			}
+			
+			System.out.println(ans);
+		}
+		
+		sc.close();
+	}
+}
