72faa56bd33d3715c50451bfee9c14ee1b192822
dgf0020 <tmxlcl0020@naver.com>
2024-07-21 16:32:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        int a = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer[a] = i;
+                a++;
+            }
+        }
+        
+        return answer;
+    }
+}
