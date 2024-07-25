d07b1378865d794a05c8bad88f69d678e8757937
dgf0020 <tmxlcl0020@naver.com>
2024-07-25 11:29:05 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 224 ms, Memory: 18264 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int l = sc.nextInt();
+		int p = sc.nextInt();
+		int num = l * p;
+		
+		for (int i = 0; i < 5; i++) {
+			int n = sc.nextInt();
+			System.out.print(n - num + " ");
+		}
+		
+		sc.close();
+	}
+}
