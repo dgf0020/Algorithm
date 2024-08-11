f5f3f340bc1d01b338375875c2f11a069784981a
dgf0020 <tmxlcl0020@naver.com>
2024-08-11 16:49:23 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/README.md
+++ 프로그래머스/0/120853. 컨트롤 제트/README.md
@@ -1,10 +1,10 @@
 # [level 0] 컨트롤 제트 - 120853 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120853?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120853) 
 
 ### 성능 요약
 
-메모리: 74.6 MB, 시간: 0.41 ms
+메모리: 75.4 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 11일 16:38:56
+2024년 08월 11일 16:49:21
 
 ### 문제 설명
 
--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -3,22 +3,17 @@ import java.util.StringTokenizer;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int lastnum = 0;
         
-        StringTokenizer st = new StringTokenizer(s);
-		
-		while(st.hasMoreElements()) {
-			String str = st.nextToken();
-
-			
-			if (str.equals("Z")) {
-				answer -= lastnum;
-			}
-			else {
-				answer += Integer.parseInt(str);
-				lastnum = Integer.parseInt(str);
-			}
-		}
+        String str[] = s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals("Z")) {
+                answer -= Integer.parseInt(str[i - 1]);
+            }
+            else {
+                answer += Integer.parseInt(str[i]);
+            }
+        }
+        
         return answer;
     }
 }
