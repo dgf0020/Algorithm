f8e351dc85d53b9d74240b889ffd6d02439da957
dgf0020 <tmxlcl0020@naver.com>
2024-08-18 14:21:13 +0900

    [Silver IV] Title: 스택, Time: 440 ms, Memory: 34940 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			
+			switch(str) {
+			case "push":
+				int x = sc.nextInt();
+				stack.push(x);
+				break;
+			case "pop":
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(stack.pop());
+				}
+				break;
+			case "size":
+				System.out.println(stack.size());
+				break;
+			case "empty":
+				if (stack.isEmpty()) {
+					System.out.println(1);
+				}
+				else {
+					System.out.println(0);
+				}
+				break;
+			case "top":
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(stack.peek());
+				}
+				break;
+			}
+		}
+	}
+}
