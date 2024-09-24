6cb0ac1e88068585280f5170b8b3489d475e42e0
dgf0020 <tmxlcl0020@naver.com>
2024-09-24 11:07:04 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = -1;
-        
         if (num1 == num2) {
-            answer = 1;
+            return 1;
         }
-        
-        return answer;
+        else {
+            return -1;
+        }
     }
 }
