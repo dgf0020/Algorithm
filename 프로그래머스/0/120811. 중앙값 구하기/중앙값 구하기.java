f1d42d0e5b236e9a2cab283c568b237e3fade1e2
dgf0020 <tmxlcl0020@naver.com>
2024-09-24 15:56:56 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.48 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,18 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        int tmp = 0;
         
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[i] > array[j]) {
-                    tmp = array[j];
-                    array[j] = array[i];
-                    array[i] = tmp;
-                }
-            }
-        }
+        Arrays.sort(array);
         
-        return array[array.length / 2];
+        answer = array[array.length / 2];
+        
+        return answer;
     }
 }
