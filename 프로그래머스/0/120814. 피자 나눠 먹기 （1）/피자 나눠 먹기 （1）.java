b1666dbabe2e3017af0a9a37d8b6c4c0c7729cc0
dgf0020 <tmxlcl0020@naver.com>
2024-09-25 21:20:14 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if (n % 7 == 0) {
-            answer = n / 7;
+        
+        answer = n / 7;
+        
+        if (n % 7 != 0) {
+            answer++;
         }
-        else {
-            answer = n / 7 + 1;
-        }
+        
         return answer;
     }
 }
