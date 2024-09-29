292685064d9c44aed24cfb08dbada9aa1e991bc3
dgf0020 <tmxlcl0020@naver.com>
2024-09-29 18:00:10 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 6.51 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -2,34 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] mats, String[][] park) {
-		int answer = -1;
-		int ans = 0;
+        int answer = -1;
+        int rows = park.length;
+        int cols = park[0].length;
         
-		Arrays.sort(mats);
+        Arrays.sort(mats);
         
-		for (int i = mats.length - 1; i >= 0; i--) {
-			for (int j = 0; j <= park.length - mats[i]; j++) {
-				for (int k = 0; k <= park[0].length - mats[i]; k++) {
-					label_break:
-						for (int x = j; x < j + mats[i]; x++) {
-							for (int y = k; y < k + mats[i]; y++) {
-								if (!park[x][y].equals("-1")) {
-                                    ans = 0;
-									break label_break;
-								}
-								else {
-									ans++;
-								}
-								
-							}
-						}
-					if (mats[i] * mats[i] <= ans) {
-						answer = mats[i];
-                        return answer;
-					}
-				}
-			}
-		}
+        for (int i = mats.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= rows - mats[i]; j++) {
+                for (int k = 0; k <= cols - mats[i]; k++) {
+                    boolean isMats = true;
+                    lb:
+                    for (int x = j; x < j + mats[i]; x++) {
+                        for (int y = k; y < k + mats[i]; y++) {
+                            if (!park[x][y].equals("-1")) {
+                                isMats = false;
+                                break lb;
+                            }
+                        }
+                    }
+                    if (isMats) {
+                        return mats[i];
+                    }
+                }
+            }
+        }
+        
         return answer;
     }
 }
