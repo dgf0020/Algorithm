fd587de6162db46eb06fd7980bfb1a28741b8a7e
dgf0020 <tmxlcl0020@naver.com>
2024-10-13 12:06:58 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.65 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,37 +2,24 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-		int is = 1;
-		
-		if (s.charAt(s.length()-1) == ' ' ) {
-			is = 0;
-		}
-		
-		s = s.toLowerCase();
-		
-		String[] msg = s.split(" ");
-		
-		for (int i = 0; i < msg.length; i++) {
-			String a = "";
-			
-			if (msg[i].equals("")) {
-				a += " ";
-			}
-			else {
-				a += msg[i].charAt(0);
-				a = a.toUpperCase();
-				
-				a += msg[i].substring(1);
-				a += " ";
-			}
-			answer += a;
-			
-		}
-		
-		if (is == 1) {
-			answer = answer.substring(0, answer.length() - 1);
-		}
+        s = s.toLowerCase();
+        String[] arr = s.split(" ");
         
-        return answer;
+        for (String jc : arr) {
+            if (jc.equals("")) {
+                answer += " ";
+            }
+            else {
+                answer += String.valueOf(jc.charAt(0)).toUpperCase();
+                answer += jc.substring(1);
+                answer += " ";
+            }
+        }
+        // 원본자체에 제일 끝이 공백인 경우
+        if (s.charAt(s.length() - 1) == ' ') {
+            return answer;
+        }
+        
+        return answer.substring(0, answer.length() - 1);
     }
 }
