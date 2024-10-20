104f0c1d3c5791b8388ef2c08204c856d086baf6
dgf0020 <tmxlcl0020@naver.com>
2024-10-20 14:35:59 +0900

    [level 3] Title: 최고의 집합, Time: 0.35 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        int[] answer = new int[n];
+        
+        if (n > s) {
+            return new int[] {-1};
+        }
+        
+        Arrays.fill(answer, s / n);
+        
+        for (int i = 0; i < s % n; i++) {
+            answer[n - 1 - i]++;
+        }
+        
+        return answer;
+    }
+}
